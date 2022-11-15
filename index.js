703a6c58fd9a427c7db722ea7c3788912273a481
Donye7 <118218290+Donye7@users.noreply.github.com>
2022-11-15 12:20:08 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+const {Client, GatewayIntentBits} = require('discord.js')
+const mongoose = require("mongoose")
+require('dotenv/config')
+
+const messageCountSchema = require("./message-count-schema")
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildMessageReactions,
+    ],
+})
+
+client.on('ready' , () => {
+    console.log('The bot is reddy');
+
+    mongoose.connect(process.env.MONGO_URI, {
+        keepAlive: true
+    })
+})
+
+client.on('messageCreate', message => {
+    if (message.content === 'gm') {
+        message.reply('GM!')
+    }
+})
+
+client.on('messageCreate', message => {
+    if (message.content === 'GM') {
+        message.reply('GM!')
+    }
+})
+
+client.on('messageCreate', message => {
+    if (message.content === 'Gm') {
+        message.reply('GM!')
+    }
+})
+
+client.on('messageCreate', message => {
+    if (message.content === 'gM') {
+        message.reply('GM!')
+    }
+})
+
+client.on('messageCreate', message => {
+    if (message.content === 'gN') {
+        message.reply('GN.')
+    }
+})
+
+client.on('messageCreate', message => {
+    if (message.content === 'Gn') {
+        message.reply('GN.')
+    }
+})
+
+client.on('messageCreate', message => {
+    if (message.content === 'GN') {
+        message.reply('GN.')
+    }
+})
+
+client.on('messageCreate', message => {
+    if (message.content === 'gn') {
+        message.reply('GN.')
+    }
+})
+
+client.on('messageCreate', async (message) => {
+    await messageCountSchema.findOneAndUpdate({
+        _id: message.author.id
+    }, {
+        _id: message.author.id,
+        $inc: {
+            messageCount: 1,
+        }
+    }, {
+        upsert: true
+    })
+})
+
+client.login(process.env.TOKEN)
